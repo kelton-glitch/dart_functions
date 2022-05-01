7779f29362529750507dd36b6514a281850b7063
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-04-30 22:10:30 -0600

    exercise3

--- bin/mini_exercises.dart
+++ bin/mini_exercises.dart
@@ -1,11 +1,14 @@
 void main(){
-print(youAreWonderful('Bob',10));
+print(youAreWonderful(name:'Bob'));
 }
 ///EXERCISE 1
 ///
 ///including exercise 2
+///
+///changing to siuit exercise 3
 
-youAreWonderful(name, numberPeople){
-  return 'You\'re wonderful, $name. $numberPeople like you';
+youAreWonderful({required String name, 
+                int numberPeople =30}){
+  return 'You\'re wonderful, $name. $numberPeople people like you';
 }
 
